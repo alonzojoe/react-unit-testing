0dd3daece014d8406bbb83b47e7f4a47f7d36820
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-23 16:55:38 +0800

    second unit test phase

--- src/components/UserForm.test.js
+++ src/components/UserForm.test.js
@@ -15,3 +15,32 @@ test("it shows two inputs and a button", () => {
   expect(button).toBeInTheDocument();
   //what we expect to do
 });
+
+test("it calls on onSubmit when the form is submitted", () => {
+  const mock = jest.fn();
+
+  //try render the component
+  render(<UserForm onSubmit={mock} />);
+
+  //find user inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  //Simulate typing name
+  user.click(nameInput);
+  user.keyboard("Joe");
+
+  //Simulate typing email
+  user.click(emailInput);
+  user.keyboard("alonzojoe@gmail.com");
+
+  const button = screen.getByRole("button");
+
+  //Simulate submitting the form
+  user.click(button);
+  //assertion
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({
+    name: "Joe",
+    email: "alonzojoe@gmail.com",
+  });
+});
